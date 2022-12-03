5e3d1c10fb415dbfd6428e4482cc223d71616daa
wakenup <fsoce@mail.ru>
2022-12-03 10:45:15 +0300

    v0

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eb9a83cc61msh7be8a96817db081p1fa738jsnd002c8067b3d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'e88f03f7c59cab1680d260c01b9b5084';
+
